3f0574e70bcdec49ce963862d63e815ead7ee1c4
c43721 <rlahman.game@gmail.com>
2020-10-12 17:58:13 -0500

    Complete row additions

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 "use strict";
 
 const API_URL = "http://www.devcodecampmusiclibrary.com/api/music";
+const container = $("#data-table");
 
 function getInitialData() {
     $.get(API_URL, function (data) {
-        log(data);
+        appendData(data);
     })
 }
 
-function log(data) {
-    const tableRow = ``
+function appendData(data) {
+    for (const item of data) {
+        const tableRow = `
+        <td>${item.title}</td>
+        <td>${item.album}</td>
+        <td>${item.artist}</td>
+        <td>${item.genre}</td>
+        <td>${item.releaseDate}</td>`
+
+        appendRow(tableRow);
+    }
+
+}
+
+function appendRow(row) {
+    const completedRow = addTrTo(row);
+    container.append(completedRow);
 }
 
 function addTrTo(string) {
